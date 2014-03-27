de4bf07378b6c7acf3e01ddbbaf0a46655ece43c
Trevor Harron <trharron@gmail.com>
2014-03-27 17:53:05 -0400

    Started bacis data structures for MSTSolver

--- src/com/trevorharron/tsp/solvers/MSTSolver.java
+++ src/com/trevorharron/tsp/solvers/MSTSolver.java
@@ -3,6 +3,7 @@ package com.trevorharron.tsp.solvers;
 import java.util.ArrayList;
 
 import com.trevorharron.tsp.graph.Graph;
+import com.trevorharron.tsp.graph.node.Node;
 
 public class MSTSolver implements Solver {
 
@@ -11,18 +12,62 @@ public class MSTSolver implements Solver {
 	public MSTSolver(){
 		
 	}
+	
 	public MSTSolver(Graph graph){
+		this.graph = graph;
+	}
+	
+	public void setGraph(Graph graph) {
 		this.graph = graph;
 	}
 	
 	@Override
 	public ArrayList<String> solve() throws NoSolutionException{
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		ArrayList<String> result = new ArrayList<String>();
+		return result;
+	}
+	
+	private class Tree{
+		
+		TreeNode root;
+
+		Tree(TreeNode root){
+			this.root = root;
+		}
+		
+		ArrayList<String> treeWalk(){
+			TreeNode current = root;
+			return new ArrayList<String>();
+		}
+
+	}
+	
+	private class TreeNode{
+		
+		Node self;
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(Node self){
+			this.self = self;
+			this.left = null;
+			this.right = null;
+		}
+		
+		void setLeft(Node left){this.left =  new TreeNode(left);}
+		
+		void setRight(Node right){this.right =  new TreeNode(right);}
+		
+		TreeNode getLeft(){	return left;}
+		
+		TreeNode getRight(){ return right;}
+		
+		Node getSelf(){ return self;}
+		
 	}
 
-	public void setGraph(Graph graph) {
-		this.graph = graph;
-	}
+
 
 }
